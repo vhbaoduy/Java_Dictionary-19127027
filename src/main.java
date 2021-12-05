08a3732035eee3d9a5781c39ac3fe4e9c7b5a23b
Võ Hoàng Bảo Duy <63220962+vhbaoduy@users.noreply.github.com>
2021-12-05 17:52:22 +0700

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,11 @@
+/**
+ * PACKAGE_NAME
+ * Created by Duy
+ * Date 12/5/2021 - 5:48 PM
+ * Description: ...
+ */
+public class main {
+    public static void main(String[] args){
+        System.out.println("Hé llo");
+    }
+}
